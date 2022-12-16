b25c39d1dc1be0626464c2906033f673feb2188c
Hiếu Hay Ho <68949135+Ryzen54600H@users.noreply.github.com>
2022-12-17 00:58:52 +0700

    Update client.py

--- client.py
+++ client.py
@@ -757,7 +757,7 @@ class Client2:
             self.opponent_end_score = ttk.Label(self.end_game_frame, text = "", font = ("Arial", 16)).grid(column=3, row=7)
             ttk.Label(self.end_game_frame, text = "", font = ("Arial", 20)).grid(column=2, row=8)
             ttk.Label(self.end_game_frame, text = "", font = ("Arial", 20)).grid(column=2, row=9)
-            ttk.Button(self.login_frame, text="Back to main menu", command= lambda: [self.Reset(), self.change_frame(self.end_game_frame_name, self.login_frame_name)]).grid(column=2, row=10)
+            ttk.Button(self.end_game_frame, text="Back to main menu", command= lambda: [self.Reset(), self.change_frame(self.end_game_frame_name, self.login_frame_name)]).grid(column=2, row=10)
             
 
 
